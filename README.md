c8e96b13ef86b735ff17e6308104d6b18e394eda
BoDee <nyx0hemir@gmail.com>
2021-06-05 05:35:38 -0600

    went a bit further than data retrieval but oh well

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # weather-dashboard
-API - https://openweathermap.org/api/one-call-api
+API - https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API Key}
+APIKEY - bd623837758ad959f4db0c37c609a865
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,50 @@
-var searchInputEl = $("#search");
-console.log(searchInputEl);
+var searchInputEl = $("#search-city");
+var citySectionHeaderEl = $("#city-section-header");
+var currTempEl = $("#temp");
+var currWindEl = $("#wind");
+var currHumidEl = $("#humid");
+var currUviEl = $("#uvi");
+
+var citySubmitHandler = function(lat, long) {
+    citySectionHeaderEl.text(searchInputEl.val());
+    var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=minutely,hourly&appid=bd623837758ad959f4db0c37c609a865";
+    fetch(weatherApi).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                var temp = currTempEl.text() + data.current.temp + "°F";
+                var wind = currWindEl.text() + data.current.wind_speed + " mph";
+                var humid = currHumidEl.text() + data.current.humidity + "%";
+                var uvind = currUviEl.text() + data.current.uvi;
+                currTempEl.text(temp);
+                currWindEl.text(wind);
+                currHumidEl.text(humid);
+                currUviEl.text(uvind);
+                forecastDisplay(data);
+            })
+        }
+    });
+}
+
+var forecastDisplay = function(weatherObj) {
+    
+}
+
+$("#search-button").on("click", function() {
+    var geocodeApi = "http://api.positionstack.com/v1/forward?access_key=6a0a7bfe7991fb3b771c2cfee43f426b&query=";
+    var fetchGeocodeUrl = geocodeApi + searchInputEl.val();
+    fetch(fetchGeocodeUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                cityLatitude = data.data[0].latitude;
+                cityLongitude = data.data[0].longitude;
+                citySubmitHandler(cityLatitude, cityLongitude);
+            })
+        }
+        else {
+            alert("City not found. Please try again.");
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to servers.");
+    });
+})
